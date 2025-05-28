34f38abceca9826cc335b7e14d926f7da55d9956
kiet <trangiakit1010@gmail.com>
2025-05-28 13:10:32 +0700

    Save local changes before pull

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -43,7 +43,7 @@ export const router = createBrowserRouter([
     ),
   },
   {
-    path: "/reset-password",
+    path: "/reset-password/",
     element: (
       <ProtectedAuthRoute>
         <ResetPassword />
