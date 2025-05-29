81ff39b06b2f567712c83101dc96721d5d5ce1c3
kiet <trangiakit1010@gmail.com>
2025-05-29 15:41:57 +0700

    change background login register

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,53 @@
+import backgroundVideo from "@/assets/login-background.mp4";
 import LoginForm from "@/components/login/LoginForm";
 
-
 const LoginPage = () => {
-    return (
-        <div style={{ backgroundImage: `url(/images/loginbackground.png)`, backgroundSize: 'cover', minHeight: '100vh', width: '100%' }}>
-            <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', minHeight: '100vh' }}>
-                <LoginForm />
-            </div>
+  return (
+    <div
+      style={{
+        position: "relative",
+        minHeight: "100vh",
+        width: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          objectFit: "cover",
+          zIndex: 0,
+        }}
+      >
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-
-        </div >
-    )
-}
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 1,
+          width: "100%",
+          padding: "0 20px",
+        }}
+      >
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
